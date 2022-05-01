429101851b4eed25e425fc2daa6e3db5f6b268ae
Richard <td231565@gmail.com>
2022-05-01 16:02:19 +0800

    change product souvenir list to masonry

--- src/views/product/List.vue
+++ src/views/product/List.vue
@@ -11,8 +11,46 @@
         }"
         @click="currentTagKey = tag.key">{{ tag.title }}</div>
     </header>
-    <div v-if="currentTagKey === 'souvenir'" class="d-flex flex-wrap justify-content-center">
+    <!-- <div v-if="currentTagKey === 'souvenir'" class="d-flex flex-wrap justify-content-center">
       <div v-for="(item, idx) in currentList" :key="item.id" class="w-45 py-2 px-1" :class="{'me-2': idx % 2 === 0}">
+        <img :src="item.listImage[0].link" alt="" class="w-100 rounded">
+        <p class="ellipsis-1 mt-1 mb-2 fw-bold">{{ item.title }}</p>
+        <div class="d-flex justify-content-between align-items-end">
+          <div>
+            <p class="my-0 text-blue fw-bold">
+              <span class="fs-7">$ </span>
+              <span >{{ item.price }}</span>
+            </p>
+            <p class="my-0 fs-7 ls-0">已售出 {{ item.saleNum }}</p>
+          </div>
+          <button class="btn" @click="gotoDetailPage(item.id)">
+            <i class="el-icon-search me-1"></i>
+            <span>More</span>
+          </button>
+        </div>
+      </div>
+    </div> -->
+    <div
+      v-if="currentTagKey === 'souvenir'"
+      class="mx-auto"
+      v-masonry
+      transition-duration="0.3s"
+      item-selector=".grid-item"
+      column-width=".grid-size"
+      :percent-position="true"
+      :fit-width="true"
+      gutter="0">
+      <div
+        v-for="(item, idx) in currentList"
+        :key="item.id"
+        v-masonry-tile
+        class="grid-item p-1 cursor-pointer"
+        :class="{
+          'grid-size': idx === 0,
+          'pe-2': idx % 2 === 0
+        }"
+        :style="`width: ${colWidth}px`">
+        <!-- block item markup -->
         <img :src="item.listImage[0].link" alt="" class="w-100 rounded">
         <p class="ellipsis-1 mt-1 mb-2 fw-bold">{{ item.title }}</p>
         <div class="d-flex justify-content-between align-items-end">
@@ -68,22 +106,20 @@ export default {
       list: []
     }
   },
-  watch: {
-    currentTagKey: {
-      immediate: true,
-      handler(val) {
-        this.getList(1, val)
-      }
-    }
-  },
   computed: {
+    colWidth() {
+      return (document.body.clientWidth - 36) / 2
+    },
     currentList() {
       const tagName = this.tagList.find(({ key }) => key === this.currentTagKey).title
       return this.list.filter(({ category }) => category === tagName)
     }
   },
+  created() {
+    this.getList()
+  },
   methods: {
-    getList(page, param) {
+    getList() {
       this.isLoading = true
       const url = `https://pengfu-app.herokuapp.com/api/product/`
       axios.get(url).then(res => {
